385768179e3c79c765c51a300d00a39057017153
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2025-10-30 11:09:26 -0700

    fixed tabs

--- src/interactions/tabs.js
+++ src/interactions/tabs.js
@@ -18,51 +18,66 @@ export const tabs = function () {
   const EASE = 'data-ix-tabs-ease';
 
   //select all the wrap elements
-  const wraps = [...document.querySelectorAll(WRAP)];
-  if (wraps.length === 0) return;
+  const tabWraps = [...document.querySelectorAll(WRAP)];
+  if (tabWraps.length === 0) return;
   //for each tabs elements
-  wraps.forEach((wrap, componentIndex) => {
+  tabWraps.forEach((tabWrap, componentIndex) => {
+    console.log(tabWrap);
     //get options
-    let loopControls = attr(true, wrap.getAttribute(LOOP_CONTROLS));
-    let slideTabs = attr(false, wrap.getAttribute(SLIDE_TABS));
-    let autoplay = attr(0, wrap.getAttribute(AUTOPLAY));
-    let duration = attr(0.2, wrap.getAttribute(DURATION));
-    let pauseOnHover = attr(false, wrap.getAttribute(PAUSE_ON_HOVER));
-    let ease = attr('power1.out', wrap.getAttribute(EASE));
+    let loopControls = attr(true, tabWrap.getAttribute(LOOP_CONTROLS));
+    let slideTabs = attr(false, tabWrap.getAttribute(SLIDE_TABS));
+    let autoplay = attr(0, tabWrap.getAttribute(AUTOPLAY));
+    let duration = attr(0.2, tabWrap.getAttribute(DURATION));
+    let pauseOnHover = attr(false, tabWrap.getAttribute(PAUSE_ON_HOVER));
+    let ease = attr('power1.out', tabWrap.getAttribute(EASE));
 
     //get elements
-    let previousButton = wrap.querySelector(`${PREV_BTN} button`),
-      nextButton = wrap.querySelector(`${NEXT_BTN} button`),
-      toggleWrap = wrap.querySelector(PLAY_BTN),
-      toggleButton = wrap.querySelector(`${PLAY_BTN} button`),
-      buttonList = wrap.querySelector('.tab_button_list'),
-      panelList = wrap.querySelector('.tab_content_list'),
+    let previousButton = tabWrap.querySelector(`${PREV_BTN} button`),
+      nextButton = tabWrap.querySelector(`${NEXT_BTN} button`),
+      toggleWrap = tabWrap.querySelector(PLAY_BTN),
+      toggleButton = tabWrap.querySelector(`${PLAY_BTN} button`),
+      buttonList = tabWrap.querySelector('.tab_button_list'),
+      panelList = tabWrap.querySelector('.tab_content_list'),
       animating = false,
+      canPlay = true,
       autoplayTl;
 
-    //function to facilitate cms lists
+    function flattenDisplayContents(slot) {
+      if (!slot) return;
+      let child = slot.firstElementChild;
+      while (child && child.classList.contains('u-display-contents')) {
+        while (child.firstChild) {
+          slot.insertBefore(child.firstChild, child);
+        }
+        slot.removeChild(child);
+        child = slot.firstElementChild;
+      }
+    }
+    flattenDisplayContents(buttonList);
+    flattenDisplayContents(panelList);
+
     function removeCMSList(slot) {
       const dynList = Array.from(slot.children).find((child) =>
         child.classList.contains('w-dyn-list')
       );
       if (!dynList) return;
-      const nestedItems = dynList?.firstElementChild?.children;
+      const nestedItems = dynList?.querySelector('.w-dyn-items')?.children;
       if (!nestedItems) return;
       const staticWrapper = [...slot.children];
-      [...nestedItems].forEach(
-        (el) => el.firstElementChild && slot.appendChild(el.firstElementChild)
-      );
+      [...nestedItems].forEach((el) => {
+        const c = [...el.children].find((c) => !c.classList.contains('w-condition-invisible'));
+        c && slot.appendChild(c);
+      });
       staticWrapper.forEach((el) => el.remove());
     }
     removeCMSList(buttonList);
     removeCMSList(panelList);
 
-    //setup tab links and content
     let buttonItems = Array.from(buttonList.children);
     let panelItems = Array.from(panelList.children);
 
     if (!buttonList || !panelList || !buttonItems.length || !panelItems.length) {
-      console.warn('Missing elements in:', wrap);
+      console.warn('Missing elements in:', tabWrap);
       return;
     }
 
@@ -79,18 +94,15 @@ export const tabs = function () {
     buttonItems.forEach((btn) => btn.setAttribute('role', 'tab'));
     panelItems.forEach((panel) => panel.setAttribute('role', 'tabpanel'));
 
-    //set index of active item
     let activeIndex = 0;
-    //function to make tab active
     const makeActive = (index, focus = false, animate = true, pause = true) => {
       if (animating) return;
       buttonItems.forEach((btn, i) => {
-        btn.classList.toggle(ACTIVE_CLASS, i === index);
+        btn.classList.toggle('is-active', i === index);
         btn.setAttribute('aria-selected', i === index ? 'true' : 'false');
         btn.setAttribute('tabindex', i === index ? '0' : '-1');
       });
-      panelItems.forEach((panel, i) => panel.classList.toggle(ACTIVE_CLASS, i === index));
-      if (typeof ScrollTrigger !== 'undefined') ScrollTrigger.refresh();
+      panelItems.forEach((panel, i) => panel.classList.toggle('is-active', i === index));
       if (nextButton) nextButton.disabled = index === buttonItems.length - 1 && !loopControls;
       if (previousButton) previousButton.disabled = index === 0 && !loopControls;
       if (focus) buttonItems[index].focus();
@@ -100,13 +112,16 @@ export const tabs = function () {
       if (activeIndex > index) direction = -1;
 
       if (typeof gsap !== 'undefined' && animate && activeIndex !== index) {
-        if (autoplayTl && typeof autoplayTl.restart === 'function') {
+        if (autoplayTl && !canPlay && typeof autoplayTl.restart === 'function') {
           autoplayTl.restart();
         }
         animating = true;
         let tl = gsap.timeline({
-          onComplete: () => (animating = false),
-          defaults: { duration: duration, ease: ease },
+          onComplete: () => {
+            animating = false;
+            if (typeof ScrollTrigger !== 'undefined') ScrollTrigger.refresh();
+          },
+          defaults: { duration: duration, ease: 'power1.out' },
         });
         if (slideTabs) {
           tl.set(currentPanel, { display: 'block', position: 'relative' });
@@ -143,20 +158,20 @@ export const tabs = function () {
     previousButton?.addEventListener('click', () => updateIndex(-1));
 
     buttonItems.forEach((btn, index) => {
-      let tabId = wrap.getAttribute('data-tab-component-id');
+      let tabId = tabWrap.getAttribute('data-tab-component-id');
       tabId = tabId ? tabId.toLowerCase().replaceAll(' ', '-') : componentIndex + 1;
       let itemId = btn.getAttribute('data-tab-item-id');
       itemId = itemId ? itemId.toLowerCase().replaceAll(' ', '-') : index + 1;
 
       btn.setAttribute('id', 'tab-button-' + tabId + '-' + itemId);
       btn.setAttribute('aria-controls', 'tab-panel-' + tabId + '-' + itemId);
-      panelItems[index].setAttribute('id', 'tab-panel-' + tabId + '-' + itemId);
-      panelItems[index].setAttribute('aria-labelledby', btn.id);
+      panelItems[index]?.setAttribute('id', 'tab-panel-' + tabId + '-' + itemId);
+      panelItems[index]?.setAttribute('aria-labelledby', btn.id);
 
       if (new URLSearchParams(location.search).get('tab-id') === tabId + '-' + itemId)
         makeActive(index),
           (autoplay = 0),
-          wrap.scrollIntoView({ behavior: 'smooth', block: 'start' }),
+          tabWrap.scrollIntoView({ behavior: 'smooth', block: 'start' }),
           history.replaceState(
             {},
             '',
@@ -171,7 +186,7 @@ export const tabs = function () {
 
     if (autoplay !== 0 && typeof gsap !== 'undefined') {
       autoplayTl = gsap.timeline({ repeat: -1 }).fromTo(
-        wrap,
+        tabWrap,
         { '--progress': 0 },
         {
           onComplete: () => updateIndex(1, false, false),
@@ -183,8 +198,7 @@ export const tabs = function () {
       let isHovered = false,
         hasFocusInside = false,
         prefersReducedMotion = false,
-        inView = true,
-        canPlay = true;
+        inView = true;
       function updateAuto() {
         if (prefersReducedMotion || !inView || canPlay || isHovered || hasFocusInside)
           autoplayTl.pause();
@@ -204,28 +218,30 @@ export const tabs = function () {
       function handleMotionChange(e) {
         prefersReducedMotion = e.matches;
         updateAuto();
+        canPlay = !e.matches;
+        setButton();
       }
       handleMotionChange(window.matchMedia('(prefers-reduced-motion: reduce)'));
       window
         .matchMedia('(prefers-reduced-motion: reduce)')
         .addEventListener('change', handleMotionChange);
       if (pauseOnHover)
-        wrap.addEventListener('mouseenter', () => {
+        tabWrap.addEventListener('mouseenter', () => {
           isHovered = true;
           updateAuto();
         });
       if (pauseOnHover)
-        wrap.addEventListener('mouseleave', () => {
+        tabWrap.addEventListener('mouseleave', () => {
           hasFocusInside = false;
           isHovered = false;
           updateAuto();
         });
-      wrap.addEventListener('focusin', () => {
+      tabWrap.addEventListener('focusin', () => {
         hasFocusInside = true;
         updateAuto();
       });
-      wrap.addEventListener('focusout', (e) => {
-        if (!e.relatedTarget || !wrap.contains(e.relatedTarget)) {
+      tabWrap.addEventListener('focusout', (e) => {
+        if (!e.relatedTarget || !tabWrap.contains(e.relatedTarget)) {
           hasFocusInside = false;
           updateAuto();
         }
@@ -236,7 +252,7 @@ export const tabs = function () {
           updateAuto();
         },
         { threshold: 0 }
-      ).observe(wrap);
+      ).observe(tabWrap);
     }
   });
 };
